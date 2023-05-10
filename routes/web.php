629c10cb0c34fc4a949ac731988b732d6d6d4640
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-10 17:32:36 +0200

    route:resource

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ComicController;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get('/', [PageController::class, 'index']);
+
+Route::resource('comics', ComicController::class);
