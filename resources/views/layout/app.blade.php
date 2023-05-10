97036faaba2e5c8962d2a2cb92cf8ac15c8e97ac
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-10 18:04:44 +0200

    h1 text center

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dc Comics</title>
+    @vite('resources/js/app.js')
 </head>
 <body>
     <header>
-        <h1>@yield('page.title')</h1>
+        <h1 class="text-center">@yield('page.title')</h1>
     </header>
     <main>
         @yield('page.main')
