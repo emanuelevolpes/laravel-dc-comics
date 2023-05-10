f7a76d440d19856baa0f48542a9c8a1cf6496177
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-10 17:48:08 +0200

    layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dc Comics</title>
+</head>
+<body>
+    <header>
+        <h1>@yield('page.title')</h1>
+    </header>
+    <main>
+        @yield('page.main')
+    </main>
+    <footer></footer>
+</body>
+</html>
