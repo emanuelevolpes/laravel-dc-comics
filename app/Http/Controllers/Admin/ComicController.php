1705ba95723b8a8f48d0496b26653c235446e820
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-10 17:52:05 +0200

    return view comic.index

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Comic;
 
 class ComicController extends Controller
 {
@@ -14,7 +15,9 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comic::all();
+
+        return view('comic.index', compact('comics'));
     }
 
     /**
