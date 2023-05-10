fd11bbdf005ffe6ac3b671dac91510ac79712c74
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-10 19:38:38 +0200

    crud store

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -37,7 +37,23 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $newComic = new Comic();
+
+        $newComic->title = $data['title'];
+        $newComic->description = $data['description'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+        $newComic->artists = json_encode($data['artists']);
+        $newComic->writers = json_encode($data['writers']);
+        $newComic->save();
+
+        return redirect()->route('comics.show', $newComic->id);
+
     }
 
     /**
--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -50,6 +50,5 @@
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
-
     </div>
 @endsection
