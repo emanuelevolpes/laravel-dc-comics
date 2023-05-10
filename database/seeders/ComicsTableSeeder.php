be461b7ef3b747ea01c176358a86d3c46eab8738
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-10 16:35:38 +0200

    complete seeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +16,22 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics'); //read comics.php file with config
+
+        foreach($comics as $comic){
+            $newComic = new Comic();
+
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->artists = $comic['atrists'];
+            $newComic->writers = $comic['writers'];
+            $newComic->save();
+        }
+
     }
 }
