3317d18c4a55df1ac2b5384c4bb9679a72328872
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-11 09:43:58 +0200

    home page btn link to comic.index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Laravel Dc Comics</title>
          @vite('resources/js/app.js')
     </head>
     <body>
+        <div class="container text-center">
+            <h1>This is the Home page</h1>
+            <a href="{{ route('comics.index') }}" class="btn btn-primary">Go to Comics list</a>
+        </div>
     </body>
 </html>
