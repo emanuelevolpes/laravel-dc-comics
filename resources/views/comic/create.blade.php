7f16de74883b154bc55dd8f1c94d3dd0d30c3e71
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-11 15:14:03 +0200

    price input type from number to text

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -26,7 +26,7 @@
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
-                <input type="number" class="form-control" id="price" name="price">
+                <input type="text" class="form-control" id="price" name="price">
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
